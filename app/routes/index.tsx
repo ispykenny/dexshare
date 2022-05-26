ea2705ad02ab20e9ce64c92857eb33f2c93fb5f4
Kenny Krosky <kenny.krosky@gmail.com>
2022-05-25 17:17:37 -0700

    fetching tokens ✅

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,30 +1,7 @@
-import {
-  LoaderFunction,
-  json
-} from '@remix-run/node';
-import { useLoaderData } from "@remix-run/react";
-import {
-  CLIENT_ID,
-  REDIRECT_URI
-} from '~/utils/constants';
-
-export const loader:LoaderFunction = () => {
-  console.log(process.env.CLIENT_ID)
-  return json(
-    {
-      ENV: {
-        client_id: process.env.ClIENT_ID
-      }
-    }
-  )
-}
-
 export default function Index() {
-  const data = useLoaderData();
-
   return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.4" }}>
-      <h1>DexShare</h1> {JSON.stringify(data)}
-    </div>
+    <>
+      <h1>DexShare</h1>
+    </>
   );
 }
